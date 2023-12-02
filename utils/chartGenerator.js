90c446d07f6cf0ae140fab77825736fd128db610
thakurshabnam231 <thakurshabnam231@gmail.com>
2023-12-02 16:59:32 +0530

    renderf charts based on rows

--- utils/chartGenerator.js
+++ utils/chartGenerator.js
@@ -3,22 +3,29 @@ const { createCanvas, loadImage } = require("canvas");
 
 async function generateChart(chartDataArray) {
   const aspectRatio = 2.75; // Set your desired aspect ratio
-  const chartCount = chartDataArray.length;
-  const widthPerChart = 140; // Set the width of each individual chart
+  const totalCharts = chartDataArray.length;
   const spacing = 20;
-  // Maximum number of rows
 
-//   const totalWidth = chartCount * (widthPerChart + spacing) - spacing;
-//   const canvasWidth = totalWidth > 500 ? totalWidth : 500;
-//   const canvasHeight = Math.round(canvasWidth / aspectRatio);
+  // Set widthPerChart and fontSize based on the number of charts
+  let widthPerChart, fontSize;
+  if (totalCharts === 1) {
+    widthPerChart = 350;
+    fontSize = 24;
+  } else if (totalCharts <= 2) {
+    widthPerChart = 240;
+    fontSize = 20;
+  } else if (totalCharts <= 6) {
+    widthPerChart = 200;
+    fontSize = 18;
+  }
 
-const canvasWidth=600;
-const canvasHeight=900;
+  const canvasWidth = 600;
+  const canvasHeight = 900;
 
   const canvas = createCanvas(canvasWidth, canvasHeight);
   const ctx = canvas.getContext("2d");
 
-  await drawCanvas(ctx, chartDataArray, canvas);
+  await drawCanvas(ctx, chartDataArray, canvas, widthPerChart, fontSize);
 
   const imageDataURL = canvas.toDataURL("image/png");
   const dataURLParts = imageDataURL.split(",");
@@ -27,18 +34,11 @@ const canvasHeight=900;
   return imageBuffer;
 }
 
-async function drawCanvas(ctx, chartDataArray, canvas) {
-  const widthPerChart = 140;
+async function drawCanvas(ctx, chartDataArray, canvas, widthPerChart, fontSize) {
   const height = 250;
   const spacing = 70;
-  const spacingY=15;
+  const spacingY = 15;
 
-  // Move gradient declaration outside of the afterDatasetsDraw function
-  const gradient = ctx.createLinearGradient(0, 0, widthPerChart, 0);
-  gradient.addColorStop(0, "green");
-  gradient.addColorStop(0.7, "yellow");
-  gradient.addColorStop(1, "red");
-  // Create an array to store all generated chart images
   const chartImages = [];
 
   for (const chartData of chartDataArray) {
@@ -46,62 +46,66 @@ async function drawCanvas(ctx, chartDataArray, canvas) {
     let min_range = range[0];
     let max_range = range[range.length - 1];
 
-    const ranges=[min_range.min, max_range.max]
+    const ranges = [min_range.min, max_range.max];
+
+    const gradient = ctx.createLinearGradient(0, 0, widthPerChart, 0);
+    gradient.addColorStop(0, "green");
+    gradient.addColorStop(0.7, "yellow");
+    gradient.addColorStop(1, "red");
+
     const gaugeChartText = {
-        id: "gaugeChartText",
-        afterDatasetsDraw(chart, args, pluginOptions) {
-          const {
-            ctx,
-            data,
-            chartArea: { top, bottom, left, right, width, height },
-            scales: { r },
-          } = chart;
-          ctx.save();
-          // Apply the gradient directly to the context
-  
-          const xCoor = chart.getDatasetMeta(0).data[0].x;
-          const yCoor = chart.getDatasetMeta(0).data[0].y;
-          const score = data.datasets[0].data[0];
-          const label = data.datasets[0].label;
-  
-          function textLabel(
-            text,
-            x,
-            y,
-            fontSize,
-            textBaseLine,
-            fontWeight,
-            textAlign
-          ) {
-            ctx.font = `${fontWeight} ${fontSize}px sans-serif`;
-            ctx.fillStyle = textBaseLine;
-            ctx.textAlign = textAlign;
-            ctx.fillText(text, x, y);
-          }
-  
-          textLabel(
-            `${ranges[0]}`,
-            left,
-            yCoor + 20,
-            15,
-            "top",
-            "normal",
-            "left"
-          );
-          textLabel(
-            `${ranges[1]}`,
-            right,
-            yCoor + 20,
-            15,
-            "top",
-            "normal",
-            "right"
-          );
-          textLabel(label, xCoor, yCoor + 40, 16, "bottom", "700", "center");
-          textLabel(score, xCoor, yCoor - 15, 25, "bottom", "bold", "center");
-        },
-      };
-  
+      id: "gaugeChartText",
+      afterDatasetsDraw(chart, args, pluginOptions) {
+        const {
+          ctx,
+          data,
+          chartArea: { top, bottom, left, right, width, height },
+          scales: { r },
+        } = chart;
+        ctx.save();
+
+        const xCoor = chart.getDatasetMeta(0).data[0].x;
+        const yCoor = chart.getDatasetMeta(0).data[0].y;
+        const score = data.datasets[0].data[0];
+        const label = data.datasets[0].label;
+
+        function textLabel(
+          text,
+          x,
+          y,
+          fontSize,
+          textBaseLine,
+          fontWeight,
+          textAlign
+        ) {
+          ctx.font = `${fontWeight} ${fontSize}px sans-serif`;
+          ctx.fillStyle = textBaseLine;
+          ctx.textAlign = textAlign;
+          ctx.fillText(text, x, y);
+        }
+
+        textLabel(
+          `${ranges[0]}`,
+          left,
+          yCoor + 20,
+          15,
+          "top",
+          "normal",
+          "left"
+        );
+        textLabel(
+          `${ranges[1]}`,
+          right,
+          yCoor + 20,
+          15,
+          "top",
+          "normal",
+          "right"
+        );
+        textLabel(label, xCoor, yCoor + 40, 16, "bottom", "700", "center");
+        textLabel(score, xCoor, yCoor - 15, 25, "bottom", "bold", "center");
+      },
+    };
 
     const canvasRenderService = new ChartJSNodeCanvas({
       width: widthPerChart,
@@ -109,7 +113,7 @@ async function drawCanvas(ctx, chartDataArray, canvas) {
       chartCallback: (ChartJS) => {
         if (ChartJS.defaults?.global) {
           ChartJS.defaults.global.defaultFontColor = "black";
-          ChartJS.defaults.global.defaultFontSize = 16;
+          ChartJS.defaults.global.defaultFontSize = fontSize;
         }
       },
     });
@@ -117,7 +121,6 @@ async function drawCanvas(ctx, chartDataArray, canvas) {
     const configuration = {
       type: type,
       data: {
-       // labels: [min_range.min, max_range.max],
         datasets: [
           {
             label: title,
@@ -127,8 +130,6 @@ async function drawCanvas(ctx, chartDataArray, canvas) {
             cutout: "72%",
             circumference: 180,
             rotation: 270,
-            // circumference: 220,
-            // rotation: 255,
           },
         ],
       },
@@ -151,29 +152,27 @@ async function drawCanvas(ctx, chartDataArray, canvas) {
           text: title,
           fontSize: 16,
         },
-        
       },
       plugins: [gaugeChartText],
-    
     };
 
     const image = await canvasRenderService.renderToBuffer(configuration);
     chartImages.push(image);
   }
 
-  // Draw the chart images on the main canvas
-  const chartsPerRow = 3; // Number of charts in each row
-  const maxRows = 4;
-  
+  const chartsPerRow =
+    chartDataArray.length >= 6 ? 2 : chartDataArray.length > 2 ? 2 : 1;
 
-  for (let i = 0; i < Math.min(maxRows, Math.ceil(chartImages.length / chartsPerRow)); i++) {
+  const maxRows = Math.ceil(chartDataArray.length / chartsPerRow);
+
+  for (let i = 0; i < maxRows; i++) {
     for (let j = 0; j < chartsPerRow; j++) {
       const chartImageIndex = i * chartsPerRow + j;
 
-      if (chartImageIndex < chartImages.length) {
+      if (chartImageIndex < chartDataArray.length) {
         const chartImage = chartImages[chartImageIndex];
         const chartImageObj = await loadImage(chartImage);
-        const offsetY = i * (90 + spacing); // Use fixed height here
+        const offsetY = i * (height + spacingY);
         const offsetX = j * (widthPerChart + spacing);
         ctx.drawImage(chartImageObj, offsetX, offsetY);
       }
